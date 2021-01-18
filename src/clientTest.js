58184f1ba10aa920dbb5c09052818b54c5f652b2
Adrien <69353057+adrien4g@users.noreply.github.com>
2021-01-18 19:02:24 -0300

    Delete clientTest.js

--- src/clientTest.js
+++
@@ -1,8 +0,0 @@
-//Load .env variables
-const dotenv = require('dotenv').config()
-
-const io = require('socket.io-client')
-
-const socket = io(`http://localhost:${process.env.SERVER_PORT}`)
-
-socket.on('newMessage', (data)=>console.log(data))
